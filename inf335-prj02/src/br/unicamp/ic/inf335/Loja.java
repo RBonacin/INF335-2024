8c52bac88a8677a11c7098f183a0b6620508127b
Rodrigo Bonacin <30866458+RBonacin@users.noreply.github.com>
2023-04-21 11:41:10 -0300

    Update Loja.java

--- inf335-prj02/src/br/unicamp/ic/inf335/Loja.java
+++ inf335-prj02/src/br/unicamp/ic/inf335/Loja.java
@@ -1,1 +1,21 @@
+package br.unicamp.ic.inf335;
 
+import java.util.ArrayList;
+
+public class Loja {
+	 
+	public static void main(String[] args) {
+		ArrayList<Produto> produtos = new ArrayList<Produto>();
+		
+		Produto prod = new Produto("Livro 1",100);
+		produtos.add(prod);
+		prod = new Produto("Sapato ",150);
+		produtos.add(prod);
+
+		for (int i=0; i<produtos.size(); i++) {
+			System.out.println("Nome do Produto = " + produtos.get(i).getNome()
+					+ " - Valor = " + produtos.get(i).getValor());
+		}
+		
+	}
+}
