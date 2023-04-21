7890a4f00ee21d251eba806cf27475bd8a9b4eb1
Rodrigo Bonacin <30866458+RBonacin@users.noreply.github.com>
2023-04-21 11:47:13 -0300

    Update Produto.java

--- inf335-prj02/src/br/unicamp/ic/inf335/Produto.java
+++ inf335-prj02/src/br/unicamp/ic/inf335/Produto.java
@@ -1,1 +1,24 @@
+package br.unicamp.ic.inf335;
+
+public class Produto {
+	private String Nome;
+	private int Valor;
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	public Produto(String nome, int valor) {
+		super();
+		Nome = nome;
+		Valor = valor;
+	}
+	public int getValor() {
+		return Valor;
+	}
+	public void setValor(int valor) {
+		Valor = valor;
+	}
+}
 
