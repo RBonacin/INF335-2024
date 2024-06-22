ae7de65fb1156b30b9d2f812b253ea971d3df2f0
Rodrigo Bonacin <rodrigo.bonacin@gmail.com>
2024-06-22 15:50:54 -0400

    Inclusão de novo produto

--- inf335-prj02/src/br/unicamp/ic/inf335/Loja.java
+++ inf335-prj02/src/br/unicamp/ic/inf335/Loja.java
@@ -12,6 +12,9 @@ public class Loja {
 		produtos.add(prod);
 		prod = new Produto("Sapato ",150);
 		produtos.add(prod);
+		
+		prod = new Produto("prod teste ",250);
+		produtos.add(prod);
 
 		for (int i=0; i<produtos.size(); i++) {
 			System.out.println("Nome do Produto = " + produtos.get(i).getNome()
